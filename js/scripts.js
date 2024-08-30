86b6092603c22a43310495b8121e6edc7b26aa45
haruki114114 <ktc24a31e0011@edu.kyoto-tech.ac.jp>
2024-08-30 09:37:01 +0900

    上部に行くjava難航

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
 new LuminousGallery(document.querySelectorAll('.grid-gallery'), {}, {
-caption: function(trigger) {
-return trigger.querySelector('img').getAttribute('alt');
+    caption: function(trigger) {
+        return trigger.querySelector('img').getAttribute('alt');
+    }
+});
+
+const topButton = document.querySelector('.topButton');
+
+topButton.addEventListener('click', () => {
+window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+});
+});
+
+window.addEventListener('scroll', () => {
+if(window.scrollY > 300) {
+    topButton.style.display = 'block';
+} else {
+    topButton.style.display = 'none';
 }
 });
